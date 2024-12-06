8aa8f728b28f9500d8d24149b7000e31dbb5ce68
Amilcar JB <amilcarjb18@gmail.com>
2024-12-06 16:45:31 +0100

    Implementação do serviço de autenticação de usuários do chat

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/models/auth_form_data.dart';
+import 'package:chat_app/core/models/auth_form_data.dart';
 import 'package:chat_app/components/auth_form.dart';
 import 'package:flutter/material.dart';
 
@@ -12,11 +12,20 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
-    print('AuthPage...');
-    print(formData.email);
-    //setState(() => _isLoading = false);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
+      
+      if (formData.isLogin) {
+        // Login
+      } else {
+        //signup
+      }
+    } catch (error) {
+      //Tratar erro!
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
